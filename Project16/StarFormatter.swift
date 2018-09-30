2fe2a8b404335833f8afc07bda60a2e55f3a31af
Jason Wong <jawong71@gmail.com>
2018-09-29 19:00:45 -0700

    first commit

---
+++ Project16/StarFormatter.swift
@@ -0,0 +1,13 @@
+//
+//  StarFormatter.swift
+//  Project16
+//
+//  Created by Jason Wong on 2018-09-29.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class StarFormatter: Formatter {
+
+}
---
+++ Review.swift
@@ -0,0 +1,13 @@
+//
+//  Review.swift
+//  Project16
+//
+//  Created by Jason Wong on 2018-09-29.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class Review: NSObject {
+
+}
